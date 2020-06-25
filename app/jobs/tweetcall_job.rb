23daba14d4648c40a64134912f648fbdc021522f
Myriam Dubois <myriam.dubois@skema.edu>
2020-06-25 13:57:43 +0200

    twwet call working in console

--- app/jobs/tweetcall_job.rb
+++ app/jobs/tweetcall_job.rb
@@ -1,12 +1,18 @@
 class TweetcallJob < ApplicationJob
   queue_as :default
 
-  def perform
+  def perform(keyword)
+    client_setup.search("#{keyword.word}", result_type: "recent").take(3).collect do |tweet|
+      Tweet.create(content: tweet.text)
+    end
+  end
+
+  def client_setup
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "YOUR_CONSUMER_KEY"
-      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-      config.access_token        = "YOUR_ACCESS_TOKEN"
-      config.access_token_secret = "YOUR_ACCESS_SECRET"
+      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
     end
   end
 end
