f15e205aa455d4e54b68d985cb0e96e40e6e8f71
Myriam Dubois <myriam.dubois@skema.edu>
2020-07-02 19:02:18 +0200

    delete tweets ok, but not yet in AJAX

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,8 @@
 class TweetsController < ApplicationController
-
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @keyword = Keyword.find_by(id: @tweet.keyword_id)
+    @tweet.destroy
+    redirect_to keyword_path(@keyword)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
       get :refresh_tweet
     end
   end
+  resources :tweets, only: [:destroy]
 end
